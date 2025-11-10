e1bd7efa397af7611495349b3d3d0210007865e9
Siegfried4512 <205353775+Siegfried081@users.noreply.github.com>
2025-11-10 20:19:01 -0300

    Consertando gitattributes

---
+++ src/test/java/com/pravi/backend/praviproject/PraviprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pravi.backend.praviproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraviprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
