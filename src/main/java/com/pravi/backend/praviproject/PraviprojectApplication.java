d82402e0ea978f7759a0eb718b2092f8bab403c5
Siegfried4512 <205353775+Siegfried081@users.noreply.github.com>
2025-11-10 20:00:35 -0300

    Commit inicial - praviproject

---
+++ src/main/java/com/pravi/backend/praviproject/PraviprojectApplication.java
@@ -0,0 +1,13 @@
+package com.pravi.backend.praviproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraviprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraviprojectApplication.class, args);
+	}
+
+}
